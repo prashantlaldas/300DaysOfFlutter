96818035dd1a37e5933f91611b93c0b6c1afc5c3
Prashant <prashantlaldas@gmail.com>
2021-06-29 14:35:12 +0530

    2nd day class over

---
+++ lib/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+class HomePage extends StatelessWidget {
+
+ final int days = 30;
+ final String name = "prashant";
+  @override
+  Widget build(BuildContext context) {
+    return   Scaffold(
+      appBar: AppBar(
+        title: Text("My Flutter App"),),
+        body: Center (
+          child:  Container(
+            child: Text ("Welcome Prashant $days $name"),
+          ),
+        ),
+        drawer: Drawer(),
+    );
+  }
+}
